03cdc09326cc922ca53e8cbb0d7597e1bbce621b
Zixuan Feng_NYU <76710319+ZixuanFeng-NYU@users.noreply.github.com>
2023-06-02 17:56:06 -0400

    Update multi_Morgan_FP.py

--- multi_Morgan_FP.py
+++ multi_Morgan_FP.py
@@ -8,24 +8,38 @@ from multiprocessing import Pool
 from tqdm import tqdm
 
 def generate_fp_for_smiles(smiles):
-    ms = Chem.MolFromSmiles(smiles)
-    if ms is None:
-        print("Invalid SMILES")
-    fp = AllChem.GetMorganFingerprintAsBitVect(ms, 2, nBits=2048)
-    fp_arr = np.zeros((fp.GetNumBits(),), dtype=np.int8)
-    for i in range(fp.GetNumBits()):
-        if fp.GetBit(i):
-            fp_arr[i] = 1
-    fp_reshaped_array = fp_arr.reshape(1, 2048)
-    return fp_reshaped_array
+    try:
+        ms = Chem.MolFromSmiles(smiles)
+        if ms is None:
+            print("Invalid SMILES")
+            return None
+        fp = AllChem.GetMorganFingerprintAsBitVect(ms, 2, nBits=2048)
+        fp_arr = np.zeros((fp.GetNumBits(),), dtype=np.int8)
+        for i in range(fp.GetNumBits()):
+            if fp.GetBit(i):
+                fp_arr[i] = 1
+        fp_reshaped_array = fp_arr.reshape(1, 2048)
+        return fp_reshaped_array
+    except Exception as e:
+        print(f"Error generating fingerprint for SMILES: {smiles}")
+        print(str(e))
+        return None
 
 def generate_fp(row):
     index, data = row
     smi = data['smiles']
     class_1row = data['Class']
-    fp_reshaped_array = generate_fp_for_smiles(smi)
-    result =  [smi, class_1row] + fp_reshaped_array.tolist()[0]
-    return '\t'.join(map(str, result))
+    try:
+        fp_reshaped_array = generate_fp_for_smiles(smi)
+        if fp_reshaped_array is not None:
+            result =  [smi, class_1row] + fp_reshaped_array.tolist()[0]
+            return '\t'.join(map(str, result))
+        else:
+            return None
+    except Exception as e:
+        print(f"Error generating fingerprint for SMILES: {smi}")
+        print(str(e))
+        return None
 
 if __name__ == '__main__':
     print("entered in to main")
@@ -41,5 +55,6 @@ if __name__ == '__main__':
             #result = list(tqdm(pool.imap(generate_fp, reader.iterrows(), chunksize=100), total=len(reader)))
             #df_fps = df_fps.append(result)
             for result in tqdm(pool.imap(generate_fp,reader.iterrows(),chunksize=100),total=len(reader)):
-                f.write(result + '\n')
+                if result is not None:
+                    f.write(result + '\n')
     print(f"Processed {len(reader)} molecules.")
